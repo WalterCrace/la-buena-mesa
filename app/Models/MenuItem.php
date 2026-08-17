519d18ad97ba4225330955f710bdb5ae698c6153
WalterCrace <mq20003@ues.edu.sv>
2026-08-17 09:51:32 -0600

    Configuración del modelo

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class MenuItem extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'category',
+        'is_available',
+    ];
 }
